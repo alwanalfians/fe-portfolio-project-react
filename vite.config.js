6dbc74d581e19747a4567ff5aa3036f6b6b81a03
alwanalfians <alwanalfians4@gmail.com>
2025-07-28 00:47:45 +0700

    feat(visualizer): add visulizer for bundle size information

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // Opens the report in the browser after build
+      filename: "stats.html", // Output file name for the report
+    }),
+  ],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "./src/assets"),
